a25b6bcb5c239a4dac3335c3b7d075dbeb6d5f3a
IT26101466 <it26101466@my.sliit.lk>
2026-06-23 21:36:35 +0530

    Add files via upload

---
+++ Q1/IT26101466Lab2Q1.java
@@ -0,0 +1,9 @@
+public class IT26101466Lab2Q1 {
+    public static void main(String[] args) {
+        double perimeter = 100.0;
+        double length = perimeter / 3.5;
+        double width = (3.0 / 4.0) * length;
+        System.out.println("Length of the fence: " + length);
+        System.out.println("Width of the fence: " + width);
+    }
+}
