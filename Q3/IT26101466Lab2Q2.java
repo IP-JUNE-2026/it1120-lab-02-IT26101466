e293128cd2e73a0f80ce41296b38cdfb4fab8b0d
IT26101466 <it26101466@my.sliit.lk>
2026-06-24 13:59:05 +0530

    Add files via upload

---
+++ Q3/IT26101466Lab2Q2.java
@@ -0,0 +1,13 @@
+public class IT26101466Lab2Q2 {
+
+    public static void main(String[] args) {
+
+        double length = 10.0;
+        double perimeter = 4 * length;
+        double pi = 3.14;
+
+        double radius = perimeter / (2 * pi);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
